81eb2582aae58c888f1c8caac5c3c70ac094d3f9
NIDHI DHIMAN <149658882+nidhidhiman-1234@users.noreply.github.com>
2024-02-09 19:54:40 +0530

    Add files via upload

--- src/pages/counsellor/cousellorTabs.js
+++ src/pages/counsellor/cousellorTabs.js
@@ -1,54 +1,34 @@
-import React, { useState } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-// import 'react-tabs/style/react-tabs.css';
+import React from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
 
-const CounsellorTabs = () => {
-  const [activeTab, setActiveTab] = useState(0);
-
-  const handleTabChange = (index) => {
-    setActiveTab(index);
-  };
-
-  return (
-    
-    <div>
-      <Tabs selectedIndex={activeTab} onSelect={handleTabChange}>
-        <TabList>
-          <Tab>Total Calls</Tab>
-          <Tab>Total Active Time</Tab>
-          <Tab>Commulative Total Minutes</Tab>
-          <Tab>Total Chat Sessions</Tab>
-          <Tab>Total Earning</Tab>
-          <Tab>Earning of Current Month</Tab>
-        </TabList>
-
-        <TabPanel>
-          {/* Content for Total Calls Tab */}
-          <p>Total Calls received</p>
-        </TabPanel>
-        <TabPanel>
-          {/* Content for Total Active Time Tab */}
-          <p>Total Active Time</p>
-        </TabPanel>
-        <TabPanel>
-          {/* Content for Commulative Total Minutes Tab */}
-          <p>Commulative Total Minutes spent on call</p>
-        </TabPanel>
-        <TabPanel>
-          {/* Content for Total Chat Sessions Tab */}
-          <p>Total Chat Sessions</p>
-        </TabPanel>
-        <TabPanel>
-          {/* Content for Total Earning Tab */}
-          <p>Total Earning</p>
-        </TabPanel>
-        <TabPanel>
-          {/* Content for Earning of Current Month Tab */}
-          <p>Earning Of current month</p>
-        </TabPanel>
-      </Tabs>
-    </div>
-  );
+const onChange = (key: string) => {
+  console.log(key);
 };
+
+const items: TabsProps['items'] = [
+  {
+    key: '1',
+    label: 'Total Active Time',
+    children: 'Total Active Time',
+  },
+  {
+    key: '2',
+    label: 'Total Chat Sessions',
+    children: 'Total Chat Sessions',
+  },
+  {
+    key: '3',
+    label: 'Total Calls',
+    children: 'Total Calls',
+  },
+  {
+    key: '4',
+    label: 'Total Earning',
+    children: 'Total Earning',
+  },
+];
+
+const CounsellorTabs: React.FC = () => <Tabs defaultActiveKey="1" items={items} onChange={onChange} />;
 
 export default CounsellorTabs;
